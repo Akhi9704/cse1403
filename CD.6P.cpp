3847f4f8e810a2d566a1f2a31eb449e304162f78
Akhi9704 <112750099+Akhi9704@users.noreply.github.com>
2023-08-09 10:37:59 +0530

    Add files via upload

---
+++ CD.6P.cpp
@@ -0,0 +1,28 @@
+#include <conio.h>
+#include <ctype.h>
+int main();
+{
+ char a[10];
+ int flag i=1;
+ printf("/n enter an identifier:");
+ gets(a);
+ if(isalpha(a[a]))
+ flag=1;
+ elseeak;
+	}
+	i++;
+}
+if (flag=1)
+printf("/n valid identifier");
+while(a[i]!='/0')
+{
+	if(! is digit(a[i])&& ! is alpha(a[i]))
+	{
+		flag=0;
+		break;
+	}
+	i++;
+}
+if(flag=1)
+printf("/n valid identifier");
+}
